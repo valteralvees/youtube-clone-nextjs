a9a20dc69590ea2ddf756f5c0b1cf4c3c62ecd75
Valter Alves <valteralvees@outlook.com>
2021-03-07 18:37:20 -0300

    Material-UI config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
-
-import styles from '../styles/Home.module.css'
-
+import React from 'react';
+import { Button } from '@material-ui/core'
 
 export default function Home() {
   return (
     <div >
      Clone YT com NextJS + materialUI
+     <Button color="primary">Hello,World!</Button>
     </div>
   )
 }
