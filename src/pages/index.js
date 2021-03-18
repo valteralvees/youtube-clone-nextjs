ddde1032d7a02cd0569049327beee5a68908fdff
Valter <valteralvees@outlook.com>
2021-03-18 18:30:44 -0300

    Get videos from MongoDB

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Box, Grid } from '@material-ui/core';
 import Layout from 'src/components/Layout';
 import VideoCard from 'src/components/VideoCard';
+import {getVideos} from 'src/database/getVideos';
 
 function Home({data}) {
   return (
@@ -26,41 +27,43 @@ function Home({data}) {
 }
 
 export async function getStaticProps(){
-  const data = [
-    {
-      id: 1,
-      title: 'NEXT.JS: O FRAMEWORK QUE VOCÊ DEVERIA CONHECER[PARTE 1]',
-      authorId: 1,
-      authorName: 'Lucas Nhimi',
-      authorAvatar: 'avatarUrl',
-      views: 10,
-      thumb: '/thumb/next01.jpg',
-      videoUrl: 'url',
-      updatedAt: new Date(),
-    },
-    {
-      id: 2,
-      title: 'NEXT.JS: MEU QUERIDO IRMÃO NEXTJS[PARTE 2]',
-      authorId: 1,
-      authorName: 'Lucas Nhimi',
-      authorAvatar: 'avatarUrl',
-      views: 10,
-      thumb: '/thumb/next02.jpg',
-      videoUrl: 'url',
-      updatedAt: new Date(),
-    },
-    {
-      id: 3,
-      title: 'NEXT.JS: ROTAS ESTATICAS E DINÂMICAS[PARTE 3]',
-      authorId: 1,
-      authorName: 'Lucas Nhimi',
-      authorAvatar: 'avatarUrl',
-      views: 10,
-      thumb: '/thumb/next03.jpg',
-      videoUrl: 'url',
-      updatedAt: new Date(),
-    },
-  ];
+  // const data = [
+  //   {
+  //     id: 1,
+  //     title: 'NEXT.JS: O FRAMEWORK QUE VOCÊ DEVERIA CONHECER[PARTE 1]',
+  //     authorId: 1,
+  //     authorName: 'Lucas Nhimi',
+  //     authorAvatar: 'avatarUrl',
+  //     views: 10,
+  //     thumb: '/thumb/next01.jpg',
+  //     videoUrl: 'url',
+  //     updatedAt: new Date(),
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'NEXT.JS: MEU QUERIDO IRMÃO NEXTJS[PARTE 2]',
+  //     authorId: 1,
+  //     authorName: 'Lucas Nhimi',
+  //     authorAvatar: 'avatarUrl',
+  //     views: 10,
+  //     thumb: '/thumb/next02.jpg',
+  //     videoUrl: 'url',
+  //     updatedAt: new Date(),
+  //   },
+  //   {
+  //     id: 3,
+  //     title: 'NEXT.JS: ROTAS ESTATICAS E DINÂMICAS[PARTE 3]',
+  //     authorId: 1,
+  //     authorName: 'Lucas Nhimi',
+  //     authorAvatar: 'avatarUrl',
+  //     views: 10,
+  //     thumb: '/thumb/next03.jpg',
+  //     videoUrl: 'url',
+  //     updatedAt: new Date(),
+  //   },
+  // ];
+
+  const data = await getVideos();
 
   return {
     props:{
